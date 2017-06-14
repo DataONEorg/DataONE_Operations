c85495933deae2dad5c7f8ff212ac7a630ae644d
datadavev <datadavev@users.noreply.github.com>
2017-06-14 10:14:31 -0400

    added new scripts

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.10.0',
+    version='0.11.0',
 
     description='Utilities for administrative operations in DataONE',
     long_description=long_description,
@@ -106,6 +106,8 @@ setup(
              'scripts/d1mnadmin',
              'scripts/d1nodeprops',
              'scripts/d1pidinfo',
+             'scripts/d1verifytoken',
+             'scripts/d1logintoken',
              ],
 
     # To provide executable scripts, use entry points in preference to the
